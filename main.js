231f06384529c7944e1ba42995e945e5948b124e
Ed Soliman <e.georgesoliman@gmail.com>
2019-08-12 09:55:44 -0700

    display/feature updates

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
 function handleLogoButton() {
     $('main').on('click', '#logo', function(event) {
-        $('main').find('#sign_in_container, #view_workouts_container, #progress_page_container, #sign_up_container, #add_workouts_container').hide();
+        $('main').find('#sign_in_container, #view_workouts_container, #progress_page_container, #sign_up_container, #add_workouts_container, #progres_page_container').hide();
         $('main').find('#landing_page_container, #nav_container').show();
     });
 };
 
 function handleSignUpButton() {
     $('main').on('click', '#register_button', function(event) {
-        $('main').find('#landing_page_container, #sign_in_container, #view_workouts_container, #progress_page_container, #add_workouts_container').hide();
+        $('main').find('#landing_page_container, #sign_in_container, #view_workouts_container, #progress_page_container, #add_workouts_container, #progres_page_container').hide();
         $('main').find('#sign_up_container').show();
     });
 };
 
 function handleSignInButton() {
     $('main').on('click', '#sign_in_button', function(event) {
-        $('main').find('#landing_page_container, #sign_up_container, #view_workouts_container, #progress_page_container, #add_workouts_container').hide();
+        $('main').find('#landing_page_container, #sign_up_container, #view_workouts_container, #progress_page_container, #add_workouts_container, #progres_page_container').hide();
         $('main').find('#sign_in_container').show();
     });
 };
 
 function handleViewWorkoutsButton() {
     $('main').on('click', '#workouts_button', function(event) {
-        $('main').find('#landing_page_container, #sign_in_container, #sign_up_container, #progress_page_container, #add_workouts_container').hide();
+        $('main').find('#landing_page_container, #sign_in_container, #sign_up_container, #progress_page_container, #add_workouts_container, #progres_page_container').hide();
         $('main').find('#view_workouts_container').show();
     });
 };
 
 function handleAddWorkoutsButton() {
     $('main').on('click', '#add_workouts_button', function(event) {
-        $('main').find('#landing_page_container, #sign_in_container, #sign_up_container, #progress_page_container, #view_workouts_container').hide();
+        $('main').find('#landing_page_container, #sign_in_container, #sign_up_container, #progress_page_container, #view_workouts_container, #progres_page_container').hide();
         $('main').find('#add_workouts_container').show();
+    });
+};
+
+function handleProgressButton() {
+    $('main').on('click', '#progress_button', function(event) {
+        $('main').find('#landing_page_container, #sign_in_container, #sign_up_container, #progress_page_container, #view_workouts_container, #add_workouts_container').hide();
+        $('main').find('#progress_page_container').show();
     });
 };
 
@@ -39,6 +46,7 @@ function handleEdsPortfolio() {
     handleSignInButton();
     handleViewWorkoutsButton();
     handleAddWorkoutsButton();
+    handleProgressButton();
 };
 
 $(handleEdsPortfolio);
